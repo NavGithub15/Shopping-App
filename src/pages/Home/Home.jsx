ea59160075a2d04d76fce2acc48a398634bd4b17
Naveen Sharma <nav.shar000@gmail.com>
2023-03-15 19:30:08 -0400

    Imported SASS library and styled the homepage content

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,19 @@
-import "./Home.css"
+import "./Home.scss";
 export default function Home() {
-
   return (
-    <div className=" h-screen relative bg-cover bg-no-repeat bg-center hero-image" >
-
+    <div className="hero">
+      <div className="hero__container">
+        <div className="hero__content-container">
+        <h1 className="hero__content">
+        <span className="hero__text-left">
+          <span className="hero__span-0">Elevate your property </span>
+        </span>
+        <span className="hero__text-right">
+          <span className="hero__span-1">with thoughtful design</span>
+        </span>
+      </h1>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
